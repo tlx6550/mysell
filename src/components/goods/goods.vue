f076d56805bb8b134ddeeb32629d955e4e04e486
tlx6550 <912730337@qq.com>
2017-08-22 17:36:38 +0800

    goods组件 父容器为flex布局 子项flex：0 0 80px  flex：none | <' flex-grow '> <' flex-shrink >'? || <' flex-basis '>  flex-grow属性定义项目的放大比例，默认为0，即如果存在剩余空间，也不放大  flex-shrink属性定义了项目的缩小比例，默认为1，即如果空间不足，该项目将缩小。  flex-basis属性定义了在分配多余空间之前，项目占据的主轴空间（main size）。浏览器根据这个属性，计算主轴是否有多余空间。它的默认值为auto，即项目的本来大小

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>i am goods</div>
+  <div class="goods">
+    <div class="menu-wrapper"></div>
+    <div class="foods-wrapper"></div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
@@ -7,5 +10,18 @@ export default {
 }
 </script>
 <style lang="stylus" ref="stylesheet/stylus">
-
+  .goods
+    display: flex
+    position: absolute
+    top: 174px
+    bottom: 46px
+    width: 100%
+    overflow: hidden
+    background-color:#7e8c8d
+    .menu-wrapper
+      flex:0 0 80px
+      width: 80px
+      background:#f3f5f7
+    .foods-wrapper
+      display: flex
 </style>
