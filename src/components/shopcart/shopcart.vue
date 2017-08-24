f02906e9e8e3c2d69fb70db9955abc1e055825ea
tlx6550 <912730337@qq.com>
2017-08-24 10:30:35 +0800

    购物车各状态实现

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -3,28 +3,78 @@
      <div class="content">
        <div class="content-left">
          <div class="logo-wrapper">
-           <div class="logo">
-             <span class="icon-shopping_cart"></span>
+           <div class="logo" :class="{'highlight':totalCount>0}">
+             <i class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></i>
            </div>
+           <div class="num" v-show="totalCount>0">{{totalCount}}</div>
          </div>
-         <div class="price">0元</div>
+         <div class="price" :class="{'highlight':totalPrice>0}">{{totalPrice}}元</div>
          <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
        </div>
-       <div class="content-right"></div>
+       <div class="content-right">
+         <div class="pay" :class="payClass">
+           {{payDesc}}
+         </div>
+       </div>
      </div>
    </div>
 </template>
 
 <script type="text/ecmascript-6">
+  //在props里如果传入的类型为数组或者object，那么默认类型是函数
 export default {
   props:{
-    deliveryPrice:{
+    selectFoods: {
+      type: Array,
+      default() {
+        return [
+          {
+            price: 10,
+            count: 3
+          }
+        ];
+      }
+    }
+    ,deliveryPrice:{
       type:Number
       ,default:0
     }
     ,minPrice:{
       type:Number
       ,default:0
+    }
+  }
+  ,computed:{
+    totalPrice(){
+      let total = 0;
+      this.selectFoods.forEach((food)=>{
+        total += food.price * food.count;
+      })
+      return total;
+    }
+    ,totalCount(){
+    let count = 0;
+    this.selectFoods.forEach((food)=>{
+      count += food.count ;
+    })
+    return count;
+  }
+  ,payDesc(){
+      if (this.totalPrice ===0 ){
+        return `￥${this.minPrice}元起送`;
+      }else if(this.totalPrice < this.minPrice){
+        let diff = this.minPrice - this.totalPrice;
+        return `还差￥${diff}元起送`;
+      }else {
+        return '去结算'
+      }
+    }
+  ,payClass(){
+      if(this.totalPrice < this.minPrice){
+        return "not-enough";
+       }else{
+        return "enough";
+      }
     }
   }
 }
